6f89b2f9c0162d99983126852ad722a90a898b41
Peter Crawford <pcraw1027@gmail.com>
2021-01-27 19:26:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-console.log(funkyFunction)
+console.log(`The varibale has ${funkyFunction} in it`)
 var theFunk = funkyFunction
 
